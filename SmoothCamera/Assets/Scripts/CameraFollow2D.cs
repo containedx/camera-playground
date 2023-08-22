711c1a431ccffda18ba24f9061b9271955862b14
Kinga <niu.ula.a@gmail.com>
2023-08-22 20:23:19 +0200

    camera follow in 2D

---
+++ SmoothCamera/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 2.0f;
+
+    
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position;
+        targetPosition.z = transform.position.z; 
+
+        Vector3 nextPos = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+
+        // Update the camera's position
+        transform.position = nextPos;
+    }
+}
