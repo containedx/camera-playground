2a2d2786cd8b0fa9279d40a9438fbf52e6efba42
Kinga <niu.ula.a@gmail.com>
2023-08-23 16:53:31 +0200

    simple movement in 3d

---
+++ SmoothCamera/Assets/Scripts/scene3D/ActorMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    public float speed = 5.0f;
+    
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude <= 0.1f)
+            return;
+
+        characterController.Move(direction * speed * Time.deltaTime);
+    }
+}
