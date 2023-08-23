b43256b02ce64c6e23ae27d3b2b52cec180fa059
Kinga <niu.ula.a@gmail.com>
2023-08-23 18:36:41 +0200

    main menu,ui, some improvements

---
+++ SmoothCamera/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private const string SCENE_2D = "SampleScene";
+    private const string SCENE_3D = "3dScene";
+
+    public void Load2dScene()
+    {
+        ChangeScene(SCENE_2D);
+    }
+
+    public void Load3dScene()
+    {
+        ChangeScene(SCENE_3D);
+    }
+
+
+    private void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
---
+++ SmoothCamera/Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private const string MAIN_MENU = "MainScene";
+
+    private void Update()
+    {
+        HandleEscape();
+    }
+
+    private void HandleEscape()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(MAIN_MENU);
+        }
+    }
+
+}
