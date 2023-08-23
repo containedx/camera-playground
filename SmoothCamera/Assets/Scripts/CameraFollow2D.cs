5a9a2cff9dbfc4610822269be6aeb3bd62930c87
Kinga <niu.ula.a@gmail.com>
2023-08-23 11:15:17 +0200

    stop following when close

--- SmoothCamera/Assets/Scripts/CameraFollow2D.cs
+++ SmoothCamera/Assets/Scripts/CameraFollow2D.cs
@@ -7,19 +7,26 @@ public class CameraFollow2D : MonoBehaviour
     public Transform target;
     public float speed = 2.0f;
 
-    
+
     private void LateUpdate()
     {
         FollowTarget();
     }
+
 
     private void FollowTarget()
     {
         Vector3 targetPosition = target.position;
         targetPosition.z = transform.position.z;
 
-        Vector3 nextPos = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        var distance = Vector3.Distance(targetPosition, transform.position);
 
+        //Debug.Log(distance);
+
+        if (distance <= 0.1f)
+            return;
+
+        Vector3 nextPos = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
         transform.position = nextPos;
     }
 }
